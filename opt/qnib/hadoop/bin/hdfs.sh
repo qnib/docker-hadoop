b03289d68dbe791a98adcee05c9a2c4308b1d3fc
Christian Kniep <ckniep@gaikai.com>
2015-08-05 17:06:39 -0700

    version with checks in place

--- opt/qnib/hadoop/bin/hdfs.sh
+++ opt/qnib/hadoop/bin/hdfs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+source /etc/bashrc.hadoop
+
+function stop_hdfs {
+    supervisorctl stop yarn
+    echo stopping hdfs
+    su -c stop-hdfs.sh hadoop
+
+
+}
+trap "stop_hdfs; exit" HUP INT TERM EXIT
+
+
 if [ ! -d /data/hadoopdata/hdfs/namenode ];then
     echo "Formating namenode"
     chown -R hadoop: /data/hadoopdata/hdfs
@@ -16,3 +28,7 @@ fi
 # Wait for sshd to be up'n'running
 sleep 5
 su -c 'start-dfs.sh' hadoop
+
+while [ true ];do
+    sleep 1
+done
